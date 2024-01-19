43c6fc296c80d518bbfd6c1ed6941f93f943954e
mhd <m.almouhtaseb@gmail.com>
2024-01-19 15:09:33 +0300

    19-1

--- demoproject/settings.py
+++ demoproject/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = 'static/'
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'demoproject/static')
 ]
 
